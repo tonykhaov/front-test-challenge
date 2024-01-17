0ee5ec66fe20dfa958eee9c12e165b81d47f752a
Tony Khaov <tony.khaov@gmail.com>
2024-01-17 19:43:55 +0100

    feat: collect user consent

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,21 +1,54 @@
-import { Button, Checkbox, FormLabel, Input } from '@mui/material'
-import { Form } from '@remix-run/react'
+import * as React from 'react'
+import { Alert, Button, Checkbox, FormLabel, Input } from '@mui/material'
+import { ActionFunctionArgs, json } from '@remix-run/node'
+import { Form, useActionData } from '@remix-run/react'
 import { Flex, Spacer, styled } from 'styled-system/jsx'
 
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const body = await request.formData()
+
+  const name = body.get('name')
+  const email = body.get('email')
+  const receiveNewsletter = body.get('receive-newsletter') === 'on'
+  const beShownTargetedAds = body.get('be-shown-targeted-ads') === 'on'
+  const contributeToAnonymousVisitStatistics = body.get('contribute-to-anonymous-visit-statistics') === 'on'
+
+  console.log({ name, email, receiveNewsletter, beShownTargetedAds, contributeToAnonymousVisitStatistics })
+  return json({ ok: true })
+}
+
 export default function Index() {
+  const actionData = useActionData<typeof action>()
+  const formRef = React.useRef<HTMLFormElement>(null)
+
+  React.useEffect(() => {
+    if (actionData?.ok) {
+      formRef.current?.reset()
+    }
+  }, [actionData])
+
   return (
     <styled.div w="xl" mx="auto">
       <Spacer h="12" />
 
-      <Form method="POST">
+      {actionData?.ok && (
+        <>
+          <Alert>
+            <strong>Thank you for your consent!</strong> We will not share your personal information with third parties.
+          </Alert>
+          <Spacer h="4" />
+        </>
+      )}
+
+      <Form ref={formRef} method="post">
         <div>
-          <Input placeholder="Name" />
+          <Input placeholder="Name" name="name" required />
         </div>
 
         <Spacer h="4" />
 
         <div>
-          <Input type="email" placeholder="Email address" />
+          <Input type="email" placeholder="Email address" name="email" required />
         </div>
 
         <Spacer h="6" />
@@ -25,21 +58,21 @@ export default function Index() {
         <styled.div borderWidth="2">
           <div>
             <FormLabel>
-              <Checkbox />
+              <Checkbox name="receive-newsletter" />
               Receive newsletter
             </FormLabel>
           </div>
 
           <div>
             <FormLabel>
-              <Checkbox />
+              <Checkbox name="be-shown-targeted-ads" />
               Be shown targeted ads
             </FormLabel>
           </div>
 
           <div>
             <FormLabel>
-              <Checkbox />
+              <Checkbox name="contribute-to-anonymous-visit-statistics" />
               Contribute to anonymous visit statistics
             </FormLabel>
           </div>
