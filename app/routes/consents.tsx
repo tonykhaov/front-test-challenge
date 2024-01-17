baae1415a23ffbd81889d798b4cba1dd07e68622
Tony Khaov <tony.khaov@gmail.com>
2024-01-17 20:47:04 +0100

    feat: pagination

--- app/routes/consents.tsx
+++ app/routes/consents.tsx
@@ -1,12 +1,24 @@
-import { Spacer, styled } from 'styled-system/jsx'
+import { Flex, Spacer, styled } from 'styled-system/jsx'
 import { database } from '../database.server'
-import { useLoaderData } from '@remix-run/react'
-import { Table, TableBody, TableCell, TableHead, TableRow } from '@mui/material'
+import { Link, useLoaderData, useSearchParams } from '@remix-run/react'
+import { Table, TableBody, TableCell, TableFooter, TableHead, TableRow } from '@mui/material'
+import { css } from '~/styled-system/css'
+import { LoaderFunctionArgs } from '@remix-run/node'
 
-export const loader = async () => {
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url)
+  const page = Number(url.searchParams.get('page') || '1')
+  const perPage = Number(url.searchParams.get('perPage') || '2')
+
   const consentsList = database.consentsList
+  const total = consentsList.length
+  const lastPage = Math.ceil(total / perPage)
 
-  const consents = database.collectedConsents.map((collectedConsent) => {
+  const start = (page - 1) * perPage
+  const end = start + perPage
+  const paginated = database.collectedConsents.slice(start, end)
+
+  const consents = paginated.map((collectedConsent) => {
     const enabledConsents = collectedConsent.consents.filter((consent) => consent.enabled)
     const userConsentsWithDetails = enabledConsents.map((consent) => ({
       id: consent.id,
@@ -22,17 +34,25 @@ export const loader = async () => {
     }
   })
 
-  return { consents }
+  return { consents, total, lastPage }
 }
 export default function ConsentsPage() {
   const data = useLoaderData<typeof loader>()
+
+  const [searchParams] = useSearchParams()
+  const currentPage = Number(searchParams.get('page') || '1')
 
   return (
     <styled.div>
       <Spacer h="12" />
       <h1>Collected Consents</h1>
 
-      <Table>
+      <Table
+        className={css({
+          maxWidth: '5xl',
+          mx: 'auto',
+        })}
+      >
         <TableHead>
           <TableRow>
             <TableCell>Name</TableCell>
@@ -53,6 +73,36 @@ export default function ConsentsPage() {
             </TableRow>
           ))}
         </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TableCell>
+              {currentPage !== 1 ? <Link to={`?page=${currentPage - 1}`}>Previous page</Link> : null}
+            </TableCell>
+            <TableCell colSpan={2}>
+              <Flex gap="2" justifyContent="center">
+                {Array.from({ length: data.lastPage }).map((_, index) => {
+                  const page = index + 1
+                  return (
+                    <Link
+                      key={index}
+                      to={`?page=${page}`}
+                      className={css({
+                        fontWeight: currentPage === page ? 'bold' : 'normal',
+                        color: currentPage === page ? 'black' : 'inherit',
+                        textDecoration: currentPage === page ? 'underline' : 'none',
+                      })}
+                    >
+                      {page}
+                    </Link>
+                  )
+                })}
+              </Flex>
+            </TableCell>
+            <TableCell>
+              {currentPage !== data.lastPage ? <Link to={`?page=${currentPage + 1}`}>Next page</Link> : null}
+            </TableCell>
+          </TableRow>
+        </TableFooter>
       </Table>
     </styled.div>
   )
