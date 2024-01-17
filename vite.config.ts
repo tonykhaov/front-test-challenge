9f620ed932303f5fa1bd7075c9a9b267bd16a7b4
Tony Khaov <tony.khaov@gmail.com>
2024-01-18 00:09:24 +0100

    chore: setup vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { unstable_vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+/// <reference types="vitest" />
+import { unstable_vitePlugin as remix } from '@remix-run/dev'
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [remix(), tsconfigPaths()],
-});
+  plugins: [!process.env.VITEST && remix(), tsconfigPaths()],
+
+  test: {
+    globals: true,
+    setupFiles: './test/setup.ts',
+    environment: 'happy-dom',
+  },
+})
