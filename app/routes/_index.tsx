5e16cb789be9dc394cf4514d2d807ccbbf63a4ab
Tony Khaov <tony.khaov@gmail.com>
2024-01-17 19:34:24 +0100

    feat: build consent form

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,58 @@
-import { styled } from 'styled-system/jsx'
+import { Button, Checkbox, FormLabel, Input } from '@mui/material'
+import { Form } from '@remix-run/react'
+import { Flex, Spacer, styled } from 'styled-system/jsx'
 
 export default function Index() {
-  return <styled.div>Give consent</styled.div>
+  return (
+    <styled.div w="xl" mx="auto">
+      <Spacer h="12" />
+
+      <Form method="POST">
+        <div>
+          <Input placeholder="Name" />
+        </div>
+
+        <Spacer h="4" />
+
+        <div>
+          <Input type="email" placeholder="Email address" />
+        </div>
+
+        <Spacer h="6" />
+        <h1>I agree to:</h1>
+        <Spacer h="4" />
+
+        <styled.div borderWidth="2">
+          <div>
+            <FormLabel>
+              <Checkbox />
+              Receive newsletter
+            </FormLabel>
+          </div>
+
+          <div>
+            <FormLabel>
+              <Checkbox />
+              Be shown targeted ads
+            </FormLabel>
+          </div>
+
+          <div>
+            <FormLabel>
+              <Checkbox />
+              Contribute to anonymous visit statistics
+            </FormLabel>
+          </div>
+        </styled.div>
+
+        <Spacer h="8" />
+
+        <Flex justifyContent="center">
+          <Button type="submit" variant="contained">
+            Give consent
+          </Button>
+        </Flex>
+      </Form>
+    </styled.div>
+  )
 }
